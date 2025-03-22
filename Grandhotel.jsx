d89cf05dcadaa1e94820df6e4f4a244767afe64e
shalinichandranna <shalinishalini91395@gmail.com>
2025-03-22 16:04:06 +0530

    add

---
+++ Grandhotel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Grandhotel = () => {
+  return (
+    <div>
+        <img src="./grand.jpg" className="d-block w=1000px" height="1000px" alt="..." />
+      </div>
+  );
+};
+
+export default Grandhotel;
